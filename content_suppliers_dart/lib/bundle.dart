2ad00d5a3a90b963a7ebc97123933c36c4d812c3
Andrii Pyvovarov <a.pyvovarov@steel.mil.ua>
2024-11-26 17:23:59 +0200

    some cleanup

--- content_suppliers_dart/lib/bundle.dart
+++ content_suppliers_dart/lib/bundle.dart
@@ -1,13 +1,9 @@
 import 'package:content_suppliers_api/model.dart';
-import 'package:content_suppliers_dart/suppliers/animeua/animeua.dart';
 import 'package:content_suppliers_dart/suppliers/anitaku/anitaku.dart';
 import 'package:content_suppliers_dart/suppliers/anitube/anitube.dart';
 import 'package:content_suppliers_dart/suppliers/hianime/hianime.dart';
 import 'package:content_suppliers_dart/suppliers/mangadex/mangadex.dart';
 import 'package:content_suppliers_dart/suppliers/tmdb/tmdb.dart';
-import 'package:content_suppliers_dart/suppliers/uafilms/uafilms.dart';
-import 'package:content_suppliers_dart/suppliers/uakinoclub/uakinoclub.dart';
-import 'package:content_suppliers_dart/suppliers/uaserial/uaserial.dart';
 import 'package:content_suppliers_dart/suppliers/ufdub/ufdub.dart';
 
 class DartContentSupplierBundle extends ContentSupplierBundle {
@@ -23,11 +19,11 @@ class DartContentSupplierBundle extends ContentSupplierBundle {
         Anitaku(),
         HianimeSupplier(),
         MangaDexSupllier(),
-        UASerialSupplier(),
-        UAKinoClubSupplier(),
+        // UASerialSupplier(),
+        // UAKinoClubSupplier(),
         AniTubeSupplier(),
-        AnimeUASupplier(),
-        UAFilmsSupplier(),
+        // AnimeUASupplier(),
+        // UAFilmsSupplier(),
         UFDubSupplier(),
       ]);
 }
