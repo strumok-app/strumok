f6d46da5f0241aaf71319c28e5844656cce32816
Andrii Pyvovarov <a.pyvovarov@steel.mil.ua>
2025-01-20 16:34:29 +0200

    fix version guard

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:strumok/app_secrets.dart';
 import 'package:strumok/content_suppliers/content_suppliers.dart';
 import 'package:strumok/content_suppliers/ffi_suppliers_bundle_storage.dart';
 import 'package:strumok/layouts/app_theme.dart';
+import 'package:strumok/upgrade/version_guard.dart';
 import 'package:strumok/utils/tv.dart';
 import 'package:strumok/utils/error_observer.dart';
 import 'package:strumok/utils/visual.dart';
@@ -84,8 +85,9 @@ class MainApp extends StatelessWidget {
             )
           ],
         ),
-        builder: (context, child) => AppTheme(child: child! //VersionGuard(child: child!),
-            ),
+        builder: (context, child) => AppTheme(
+          child: VersionGuard(child: child!),
+        ),
       ),
     );
   }
